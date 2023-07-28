c43488ea14a4bbd82537911ad202db3520735027
naveen <naveensirsi97@gmail.com>
2023-07-28 22:24:39 +0530

    refactor: remove warnings

--- lib/product/ui/widget/product_row_widget.dart
+++ lib/product/ui/widget/product_row_widget.dart
@@ -11,46 +11,48 @@ class ProductRowWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return ProductDetailScreen(
-              productModel: productModel,
-            );
-          }));
-        },
-        child: Card(
-            surfaceTintColor: Colors.white,
-            elevation: 4,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Expanded(
-                    child: Center(
-                      child: CachedNetworkImage(
-                        imageUrl: productModel.image,
-                        placeholder: (context, url) => const SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: CircularProgressIndicator(strokeWidth: 1)),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                      ),
-                    ),
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return ProductDetailScreen(
+            productModel: productModel,
+          );
+        }));
+      },
+      child: Card(
+        surfaceTintColor: Colors.white,
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                child: Center(
+                  child: CachedNetworkImage(
+                    imageUrl: productModel.image,
+                    placeholder: (context, url) => const SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: CircularProgressIndicator(strokeWidth: 1)),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  Text(
-                    productModel.title,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                  ),
-                  Text(
-                    '\$ ${productModel.price?.toStringAsFixed(2)}',
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
+                ),
               ),
-            )));
+              Text(
+                productModel.title,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                '\$ ${productModel.price?.toStringAsFixed(2)}',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
